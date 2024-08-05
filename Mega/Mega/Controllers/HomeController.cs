363be50ca246ef2212352fc154089638b68e3549
Conannia <conannia17@gmail.com>
2024-08-05 16:06:43 +0700

    form

--- Mega/Mega/Controllers/HomeController.cs
+++ Mega/Mega/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Mega.Models;
+﻿using Mega.Areas.Identity.Data;
+using Mega.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Host.Mef;
 using System.Diagnostics;
 
 namespace Mega.Controllers
@@ -7,14 +10,19 @@ namespace Mega.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<MegaUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<MegaUser> userManager)
         {
             _logger = logger;
+            this._userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            ViewData["UserID"] = _userManager.GetUserId(this.User);
+            //ViewData["UserID"] = _userManager.GetAccessFailedCountAsync\\\\\
+
             return View();
         }
 
